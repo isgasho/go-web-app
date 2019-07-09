fc3751a76f534f47e13f8de0c1a270f1bd14c88e
talentless_guy <pilll.PL22@gmail.com>
2019-07-09 16:05:50 +0300

    update version

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func info() {
 	app.Name = "go-web-app"
 	app.Usage = "Simple CLI for setting up Go WebAssembly frontend app."
 	app.Author = "v1rtl (twitter.com/v1rtl)"
-	app.Version = "0.0.1"
+	app.Version = "0.0.2"
 }
 
 func commands() {
